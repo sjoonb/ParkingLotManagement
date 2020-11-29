a703ec156ffb4211502c6fe0abba446aa83bc4e9
sjoonb <sungjun9808@gmail.com>
2020-11-30 01:01:42 +0900

    Added fuction that save space valute to txt file

--- main.c
+++ main.c
@@ -2,14 +2,14 @@
 #include <curses.h> // system clear
 #include <stdlib.h>
 #include <string.h> //strcpy, strcmp
+#include <unistd.h> //access
 
 enum functype {ENTER = 1, EXIT, STATUS, AUTO, QUIT};
-enum car_size {SMALL, MID, LARGE};
 
 struct car {
 	char num[10];
+	char size[10];
 	int time;
-	int size;
 	struct car* next;
 };
 
@@ -17,14 +17,14 @@ typedef struct car car_t;
 
 // Enum 활용해서 기능 선택 직관화
 
-void init_space(int* pspace);
+void init_space(int* pspace, int* pnew);
 void init_cars();
 void init_rate();
 car_t* entering(car_t* main_list_head);
 void exiting();
 void status(car_t* list_head);
 void auto_mode();
-void save_and_quit();
+void save_and_quit(int new, int space);
 
 void reset(); // 초기화 함수는 숨겨놀까? password 까지 구현?
 
@@ -34,6 +34,7 @@ int main() {
 	// 포인터 변수를 통해 주차장 크기, 차량들을 init 하는게 좋을 것 같다. 
 	int func;
 	int loop;
+	int new = 0;
 	char ch;
 
 	// 우선 4자리수 차량으로 하고, 시간이 남는다면 가나다라 추가하자
@@ -42,7 +43,7 @@ int main() {
 	int space; // 주차장 크기
 	// 먼저 프로그램이 주차장 크기, 차량, 요금표를 받아와야 한다. 처음 만들어 진건지 체크는 그 안에서 이루어 지면 된다. 
 	// 우선 주차장 사이즈를 가져오고,
-	init_space(&space);
+	init_space(&space, &new);
 
 	// 그 사이즈에 맞게 struct car 연결리스트를 형성해야 한다.
 	init_cars();
@@ -65,16 +66,19 @@ int main() {
 	switch(func){
 	// TODO - 입력 오류 처리하기. 기능 외에 숫자가 들어왔을때, 무한루프가 되는 현상 해결.
 		case ENTER:
+			printf("- 입차\n");
 			main_list_head = entering(main_list_head);
 			break;
 		case EXIT:
 			break;
 		case STATUS:
+			printf("%d\n", space);
 			status(main_list_head);
 			break;
 		case AUTO:
 			break;
 		case QUIT:
+			save_and_quit(new, space);
 			break;
 		default:
 			printf("nope\n");
@@ -95,17 +99,20 @@ int main() {
 
 }
 
-void init_space(int* pspace) {
+void init_space(int* pspace, int* pnew) {	
+	FILE* fp = NULL;
 
-	// if ~~ 파일이 존재하는가? 존재하지 않는가?
-	FILE *fp = fopen("space.txt", "r");
-	if(fp == NULL) {
-	// TODO - if not,  존재하지 않을 경우 새로 space 를 정의해 줘야 한다.
+	if(access("space.txt", F_OK) == 0) {
+		// TODO - 자동차 랜덤 생성할지 묻는다. 아니면 그냥 빈칸으로 냄겨 둔다.
+		fp = fopen("space.txt", "r");
+		fscanf(fp,"%d\n", pspace); 
+
+		fclose(fp);
+
+	} else {
 		printf("주차장 최대 공간을 입력해 주세요: ");
 		scanf("%d", pspace);
-		// TODO - space.txt 파일 만들고, 방금 받은 공간의 크기값 넣어주어야 한다.
-		
-		// TODO - 자동차 랜덤 생성할지 묻는다. 아니면 그냥 빈칸으로 냄겨 둔다.
+		*pnew = 1; // 마지막에 저장할 때 사용하기 위한 변수
 	} 
 	// else
 	// TODO - 파일에서 space 와, 차량들을 갖고온다. 이 때 차량이 몇개인지 count 해준다.
@@ -119,8 +126,8 @@ void init_space(int* pspace) {
 	// init_cars()
 	// 1 ~ MAX_SPACE 사이에 수로 차량번호, 차종, 입차시간까지 랜덤으로 설정
 
-
 }
+
 
 void init_cars() {
 	
@@ -137,16 +144,22 @@ void init_rate() {
 
 car_t* entering(car_t* main_list_head) {	
 	char tmp_num[10];
+	char tmp_size[10];
 	car_t* new_node;
 	//car_t* list_head = NULL;
 	car_t* next_p;
 	car_t* prev_p;
 
-	printf("car's number: ");
+	printf("차량번호: ");
 	scanf("%s", tmp_num);
+
+	printf("소형 / 중형 / 대형\n");
+	printf("차량 사이즈: ");
+	scanf("%s", tmp_size);
 
 	new_node = (car_t*) malloc (sizeof(car_t));
 	strcpy(new_node->num, tmp_num);
+	strcpy(new_node->size, tmp_size);
 
 	next_p = main_list_head;
 	prev_p = NULL;
@@ -184,10 +197,8 @@ void exiting() {
 
 void status(car_t* list_head) {
 	
-	printf("status\n");
-
 	while(list_head != NULL){
-		printf("%s => ", list_head->num);
+		printf("%s, %s\n", list_head->num, list_head->size);
 		list_head = list_head->next;
 	}
 	printf("\n");
@@ -202,3 +213,12 @@ void auto_mode() {
 	// TODO - 랜덤하게 exiting, entering 실행
 }
 
+void save_and_quit(int new, int space){
+	if(new){
+		FILE* fp = fopen("space.txt", "w");
+		fprintf(fp, "%d", space);
+		fclose(fp);
+	}
+
+	exit(0);
+}
